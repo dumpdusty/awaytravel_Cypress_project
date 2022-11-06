2c6e62fcbbb61e4fe01bef2936910a619ad79fcb
dumpdusty <dumpdusty@gmail.com>
2022-11-06 01:02:57 -0800

    after assessment submitted add test 10.1

--- cypress/e2e/awayTest.cy.js
+++ cypress/e2e/awayTest.cy.js
@@ -71,19 +71,19 @@ describe(`Arrow icon`, function () {
 })
 
 describe(`Testing Houston store`, function () {
-    before(function(){
+    before(function () {
         cy.visit(`/stores`)
         cy.get(`[data-link="/stores/houston"]`).click()
     })
     it(`Test 5.0 check the title`, function () {
         cy.title().should(`contain`, `Away in Houston`)
     })
-    it(`Test 5.1 check description`, function(){
+    it(`Test 5.1 check description`, function () {
         cy.get(`a[href^="tel"]`).should(`be.visible`)
         cy.get(`a[href^="mailto"]`).should(`be.visible`)
         cy.get(`a[href="/help/our-stores"]`).should(`be.visible`)
     })
-    it(`Test 6.0 check additional information`, function(){
+    it(`Test 6.0 check additional information`, function () {
         cy.get(`[class^="store_location_messages_tiles"]`).should(`contain`, `Sanitizing stations`)
         cy.get(`div[class^="store_location_messages_tiles"] svg`).eq(0).invoke(`attr`, `width`)
             .should(`eq`, `80`)
@@ -126,7 +126,7 @@ describe(`Testing Chicago store`, function () {
 })
 
 describe(`Testing Gallery`, function () {
-    before(function(){
+    before(function () {
         cy.visit(`/stores/austin`)
 
         //preparation to test# 9.2
@@ -141,7 +141,7 @@ describe(`Testing Gallery`, function () {
 
     it(`Test 9.1 check the all images loaded`, function () {
         cy.get(`[class="component component-gallery"]`)
-            .find(`[class^="image_component"]`).each((el) =>{
+            .find(`[class^="image_component"]`).each((el) => {
             cy.wait(5000)
             cy.wrap(el).should('exist')
                 .and((el) => {
@@ -158,17 +158,18 @@ describe(`Testing Gallery`, function () {
     })
 
     it(`Test 9.2 check the arrow buttons are working`, function () {
-       expect(`@pic1`).to.not.equal(`@pic2`)
+        expect(`@pic1`).to.not.equal(`@pic2`)
     })
 
 })
 
-describe(`Testing map section`, function () {
+
+describe(`Check the map and address areas`, function () {
     before(function () {
         cy.visit(`/stores/austin`)
     })
 
-    it.only(`Test 10.0 store pinned on google maps`, function () {
+    it(`Test 10.0 store pinned on google maps`, function () {
         cy.get(`[aria-label="Map"]`).should(`be.visible`)
         cy.get(`[aria-label="Away in Austin: Domain Northside"]`).should('be.visible')
         cy.get(`[itemprop="addressLocality"]`).should(`contain`, `Austin`)
@@ -176,6 +177,26 @@ describe(`Testing map section`, function () {
         cy.get(`[class^="store_location_info_infoContainer"]`).should(`contain`, `Hours`)
 
     })
+})
+describe.only(`Check if user could drug the map`, function () {
+    before(function () {
+        cy.visit(`/stores/austin`)
+
+        //defining the variables for check the map moving
+        cy.get(`[aria-label="Map"]`).trigger(`mousedown`)
+        cy.get(`[style^="z-index: 1;"]`).invoke(`attr`, `style`).as(`pos1`)
+        cy.get(`[aria-label="Map"]`).trigger(`mousemove`, {which: 1, pageX: 100, pageY: 100})
+        cy.get(`[style^="z-index: 1;"]`).invoke(`attr`, `style`).as(`pos2`)
+        cy.get(`[aria-label="Map"]`).trigger(`mouseup`)
+
+        ////defining the variables for check the map zoom in
+
+    })
+    it(`Test 10.1 Drag the map`, function () {
+        expect(`@pos1`).to.not.eq(`@pos2`)
+    })
+
+
 })
 
 //Last two tasks were not completed due to lack of time.
